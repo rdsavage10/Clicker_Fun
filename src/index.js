1141e691345457da060e6c8311a56fc50319cd74
Rick <rdsavage10@gmail.com>
2020-01-24 11:31:13 -0500

    added JQuery and refactored

--- src/index.js
+++ src/index.js
@@ -1,149 +1,135 @@
+/*jshint esversion: 6 */
 // lets declare some variables
-let total = 0
-let counter = 0
-let unusedClicks = 50
-let autoBuying = false
-let multiplier = 100
-let clickCost = 5
-let upgradeCost = 10
-let catCount = 0
-let catCost = 100
-let catMultiplier = 1
-let loopTicks = 1
-let secondTimer = 1
-let minuteTimer = 1
-let secPrevTotal = 0
-let minPrevTotal = 0
-let catLevel = 0
-let prev_time = 0
-let root = document.documentElement
-let catUpgradeText = [
-  'Give cats treats for clicking',
-  'Give cats lots of pets',
-  'Buy Catnip',
-  'Increase cat treats',
-  'Make a mouse click leaderboard for which cat has the most clicks',
-]
-themes = {
-  unlocked: false,
-  theme0: {
-    mainTextColor: 'black',
-    bodyBackground: 'white',
-  },
-  theme1: {
-    cost: 200,
-    mainTextColor: 'white',
-    bodyBackground: 'black',
-  },
-  theme2: {
-    cost: 250,
-    mainTextColor: 'grey',
-    bodyBackground: 'blue',
-  },
-  theme3: {
-    cost: 300,
-    mainTextColor: 'red',
-    bodyBackground: 'white',
-  },
-  font0: "'Times New Roman', Times, serif",
-  font1:1 ,
-  font2:1 ,
-  font3:1 ,
+let total = 0;
+let counter = 0;
+let unusedClicks = 50;
+let autoBuying = false;
+let multiplier = 100;
+let clickCost = 5;
+let upgradeCost = 10;
+let catCount = 0;
+let catCost = 100;
+let catMultiplier = 1;
+let loopTicks = 1;
+let secondTimer = 1;
+let minuteTimer = 1;
+let secPrevTotal = 0;
+let minPrevTotal = 0;
+let catLevel = 0;
+let current_time;
+let current_ms;
+let prev_ms = 0;
+let prev_times = [];
+// const catUpgradeText = [
+//   'Give cats treats for clicking',
+//   'Give cats lots of pets',
+//   'Buy Catnip',
+//   'Increase cat treats',
+//   'Make a mouse click leaderboard for which cat has the most clicks',
+// ];
 
-}
+const clickButton = $("#click");
+const buyClickButton = $("#buyClicks");
+const upgradeClickButton = $("#upgradeClick");
+const clickPerSecond = $('#clickPerSecond');
+const clickPerMinute = $('#clickPerMinute');
+const autoBuySection = $('#autoBuySection');
+const autoBuyToggleSection = $('#autoBuyToggleSection');
+const autoBuyToggleButton = $('#autoBuyToggle');
+const catCountSpan = $('#catCount');
+const hireCatButton = $('#hireCat');
+const fpsSpan = $('#fps');
+const autoBuyUnlockButton = $("#autoBuyUnlock");
+const themestoreButton = $("#themestore");
 
-//shortcut for setting innerHTML so much
-function innerHTML(element, value) {
-  document.getElementById(element).innerHTML = value
-}
 
 //shortcut for changing button disabled state
-function buttonDisable(element, value) {
-  document.getElementById(element).disabled = value
+function buttonDisabled(element, bool) {
+  if (bool) {
+    element.prop("disabled", true);
+  } else {
+    element.prop("disabled", false);
+  }
 }
 
 // change active theme
-function changeTheme(theme) {
-  root.style.setProperty('--main-text-color', theme.mainTextColor)
-  root.style.setProperty('--body-background', theme.bodyBackground)
+function changeTheme(id) {
+  $("body").attr('class', '').addClass(id);
 }
-
 
 // clicking
 function increase() {
-  counter += (1 * multiplier)
-  total += (1 * multiplier)
-
-  unusedClicks--
+  counter += multiplier;
+  total += multiplier;
+  unusedClicks--;
 }
 
 // buys clicks. duh.
 function buyClicks() {
-  counter -= clickCost
-  unusedClicks += 2 * clickCost
-  clickCost += (Math.floor(clickCost * .125) + 1)
-  innerHTML("buyClicks", clickCost)
+  counter -= clickCost;
+  unusedClicks += 2 * clickCost;
+  clickCost += (Math.floor(clickCost * 0.125) + 1);
+  buyClickButton.text(clickCost);
 }
 
 //upgrades click power
 function upgradeClick() {
-  multiplier += 2
-  counter -= upgradeCost
-  upgradeCost = Math.floor(upgradeCost * 1.5)
-  innerHTML("upgradeClick", upgradeCost)
-  innerHTML("click", ("+$" + multiplier))
+  multiplier += 2;
+  counter -= upgradeCost;
+  upgradeCost = Math.floor(upgradeCost * 1.5);
+  upgradeClickButton.text(upgradeCost);
+  clickButton.text("+$" + multiplier);
 }
 
 //revenue per minute
 function clicksPerMinute() {
-  secondTimer++
-  minuteTimer++
+  secondTimer++;
+  minuteTimer++;
   if (secondTimer >= 300) {
-    innerHTML('clickPerSecond', (total - secPrevTotal / 5))
-    secPrevTotal = total
-    secondTimer = 1
+    clickPerSecond.text((total - secPrevTotal) / 5);
+    secPrevTotal = total;
+    secondTimer = 1;
   }
   if (minuteTimer >= 600) {
-    innerHTML('clickPerMinute',(total - minPrevTotal * 6))
-    minPrevTotal = total
-    minuteTimer = 1
+    clickPerMinute.text((total - minPrevTotal) * 6);
+    minPrevTotal = total;
+    minuteTimer = 1;
   }
-
 }
 
 // this auto buys clicks
 function autoBuy() {
   autoBuying = true;
-  document.getElementById('autoBuySection').hidden = true
-  document.getElementById('autoBuyToggleSection').hidden = false
+  autoBuySection.hide();
+  autoBuyToggleSection.show();
 }
 
 function autoBuyToggle() {
   if (autoBuying === true) {
     autoBuying = false;
-    counter -= 50
-    innerHTML('autoBuyToggle', "Off")
+    counter -= 50;
+    autoBuyToggleButton.text("Off");
   } else {
     autoBuying = true;
-    innerHTML('autoBuyToggle', "On")
+    autoBuyToggleButton.text("On");
   }
 }
 
 function hireCat() {
-  catCount++
-  counter -= catCost
-  catCost += 25
-  innerHTML('catCount', (catCount + " cats"))
-  innerHTML('hireCat', catCost)
+  catCount++;
+  counter -= catCost;
+  catCost += 25;
+  catCountSpan.text(catCount + " cats");
+  hireCat.text(catCost);
 }
 
 function catIncrease(count) {
   if (count > unusedClicks) {
-    count = unusedClicks
+    count = unusedClicks;
   }
-  counter += count
-  total += count
-  unusedClicks -= count
+  counter += count;
+  total += count;
+  unusedClicks -= count;
 }
 
 function catUpgrade(level) {
@@ -151,85 +137,86 @@ function catUpgrade(level) {
 }
 
 function fps() {
-  current_time = new Date()
-  current_ms = current_time.getTime()
-  average = 1000 / (current_ms - prev_time)
-  innerHTML('fps', Math.round(average))
-  prev_time = current_ms
+  current_time = new Date();
+  current_ms = current_time.getTime();
+
+  average = 1000 / current_ms - prev_ms;
+  prev_times.push(current_ms);
+  if (prev_times.length >= 100) {
+    prev_times = prev_times.reduce((a,b) => a + b) / prev_times.length;
+    // console.log(prev_time);
+
+    fpsSpan.text(Math.round(average));
+    prev_times = [];
+  }
 }
 
 function button_update() {
   //flags for disabling buttons
   if (unusedClicks < 1) {
-    buttonDisable("click", true)
+    buttonDisabled(clickButton, true);
   } else {
-    buttonDisable("click", false)
+    buttonDisabled(clickButton, false);
   }
   if (50 <= counter) {
-    buttonDisable("autoBuy", false)
-  } else {
-    buttonDisable("autoBuy", true)
+    buttonDisabled(autoBuyUnlockButton);
   }
   if (upgradeCost <= counter) {
-    buttonDisable("upgradeClick", false)
-
-  } else {
-    buttonDisable("upgradeClick", true)
+    buttonDisabled(upgradeClickButton);
   }
   if (clickCost <= counter) {
-    buttonDisable("buyClicks", false)
-  } else {
-    buttonDisable("buyClicks", true)
+    buttonDisabled(buyClickButton);
   }
   if (catCost <= counter) {
-    buttonDisable("hireCat", false)
-  } else {
-    buttonDisable("hireCat", true)
+    buttonDisabled(hireCatButton);
   }
-  if (themes.theme1.cost <= counter) {
-    buttonDisable("themestore", false)
-  } else {
-    buttonDisable("themestore", true)
-  }
+  // if (themes.theme1.cost <= counter) {
+  //   buttonDisabled(themestore)
+  // }
 }
 
 // main game loop
 function paint() {
-  button_update()
+  button_update();
   // temporary fps counter
-  fps()
-  clicksPerMinute()
+  fps();
+  clicksPerMinute();
   //auto buying unused clicks
   if (autoBuying === true) {
     if (counter > clickCost && unusedClicks <= 30) {
-      buyClicks()
+      buyClicks();
     }
   }
 
   // cat section (auto clicking)
   if (catCount !== 0) {
     if (loopTicks < (100)) {
-      loopTicks++
+      loopTicks++;
     } else {
-      catIncrease(catCount * catMultiplier)
-      loopTicks = 1
+      catIncrease(catCount * catMultiplier);
+      loopTicks = 1;
     }
 
   } else if (counter >= catCost) {
-    document.getElementById('catZone').hidden = false
+    $('#catZone').show();
   }
 
   // unhiding theme section
-  if (document.getElementById('themestore').hidden === true && counter >= 150) {
-    document.getElementById("themestore").hidden = false
+  if ($('#themestore').is(":hidden") && counter >= 150) {
+    $("#themestore").show();
   }
 
   // updating numbers on screen
-  innerHTML("count", counter)
-  innerHTML("unusedClicks", unusedClicks)
-  window.requestAnimationFrame(paint)
-
+  $("#wallet").text(counter);
+  $("#unusedClicks").text(unusedClicks);
+  window.requestAnimationFrame(paint);
 }
 
 
-window.requestAnimationFrame(paint)
+/* add all event listeners */
+$(document).ready(function() {
+  $('button.theme-button').on('click', function(e) {
+    changeTheme(e.currentTarget.id);
+  });
+  window.requestAnimationFrame(paint);
+});
